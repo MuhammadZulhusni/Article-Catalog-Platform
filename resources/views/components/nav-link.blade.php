513f7edd8adacafdb2bf24734b5e37206a4e827c
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-16 23:58:13 +0800

    #6 GIT BRANCH & MERGE

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => ($active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white') . ' rounded-md px-3 py-2 text-sm font-medium']) }} aria-current="{{ $active ? 'page' : '' }}">
+    {{ $slot }}
+</a>
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/home', function () {
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'zulhusni']); // Send data array to view about
+    return view('about', ['name' => 'zulhusni', 'title' => 'About Page']); // Send data array to view about
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog Page']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact Page']);
 });
