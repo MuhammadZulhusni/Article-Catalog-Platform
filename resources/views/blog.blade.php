b66c8354575051f2eaac0b6d3cdfb0108ac270b7
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-17 23:49:09 +0800

    View Data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-Layout> <!-- panggil Layout component -->
-    <x-slot:title> {{ $title }} </x-slot:title> <!-- Setkan variable title -->
-    <h3 class="text-xl">Welcome to my blog!</h3>
-</x-Layout>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<x-Layout> <!-- panggil Layout component -->
+    <x-slot:title> {{ $title }} </x-slot:title> <!-- Setkan variable title -->
+
+    <!-- $posts array from web.php and assign here as a post-->
+
+    <article class="py-8 max-w-screen-md ">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>    
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a> <!-- laquo means left arrow -->
+    </article>
+
+</x-Layout>
