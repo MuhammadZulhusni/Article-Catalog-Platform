b40cd02f4b8557653bef886301c4d64f2a8c4e64
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-20 18:20:13 +0800

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,41 +1,12 @@
-<?php 
+<?php
 
-namespace App\Models; // Ni wajib letak, ad apply it dlm web.php
-use Illuminate\Support\Arr;
+namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'title' => 'Article 1',
-                'slug' => 'article-1',
-                'author' => 'zulhusni',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae pariatur dolor hic ipsum, illo est? Amet libero quo eligendi modi quis! Dolorem sunt quasi eveniet, placeat earum at dolore obcaecati?',
-            ],
-            [
-                'id' => '2',
-                'slug' => 'article-2',
-                'title' => 'Article 2',
-                'author' => 'zulhusni',
-                'body' => 'Lorem2 ipsum, dolor sit amet consectetur adipisicing elit. Molestiae pariatur dolor hic ipsum, illo est? Amet libero quo eligendi modi quis! Dolorem sunt quasi eveniet, placeat earum at dolore obcaecati?',
-            ],
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug );  // Arrow function
-        // dd($slug);  Nk test data guna command ni dlu
-
-        // Ni nk buat page 404 if route unavailable
-        if(!$post){
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
-
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
         </a>
 
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>     <!-- Guna string limit -->
         <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
