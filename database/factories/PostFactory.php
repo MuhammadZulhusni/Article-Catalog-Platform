36801f2a68dd7d2587a6508a9c8d3868585f1cf3
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-20 22:55:54 +0800

    Model Factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'author' => fake()->name(),
+            'slug' => Str::slug(fake()->sentence()),
+            'body' => fake()->text()
+        ];
+    }
+}
