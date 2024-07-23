1cc33dbda87e47866ce70e293eec28d88c3db954
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-23 23:07:33 +0800

    style:Applied Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {
@@ -14,5 +15,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [require("flowbite/plugin")],
 };
