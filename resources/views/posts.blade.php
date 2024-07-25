beb2181099faa4d11457ad7499c3718ed9effbde
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-25 23:51:32 +0800

    feat:Searching

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,28 @@
 <x-Layout> <!-- panggil Layout component -->
     <x-slot:title> {{ $title }} </x-slot:title> <!-- Setkan variable title & nnti blh guna dkt web.php-->
 
+        <!-- Searching -->
+        <div class="py-4 px-4 mx-auto max-w-screen-xl lg:px-6">
+            <div class="mx-auto max-w-screen-md sm:text-center">
+                <form action="/posts">
+                    <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                        <div class="relative w-full">
+                            <label for="search" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
+                            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+
+                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"> <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/> </svg>
+                            
+                            </div>
+                            <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search for article" type="search" id="search" name="search" autocomplete="off">
+                        </div>
+                        <div>
+                            <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
         <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-0">
             <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
             <!-- Div ni kene letak luar foreach -->
