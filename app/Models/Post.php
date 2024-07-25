ccd2140a7747b48aaf1de23fc9cc2d6664448968
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-26 00:45:23 +0800

    perf:Search complex

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -24,4 +25,19 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where('title', 'like', '%' . $search . '%')
+        );
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn($query) => $query->where('slug', $category))
+        );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn($query) => $query->where('username', $author))
+        );
+    }    
 }
